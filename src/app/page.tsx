7a35c9effb78a0eeb91e68003b67fc287e55c845
Divyansh Gupta <divyanshgupta6170@gmail.com>
2025-11-12 01:24:22 +0530

    added banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Banner from "@/components/Banner";
+// import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      {/* <h1>hi this is h1</h1> */}
+      <Banner />
     </>
   );
 }

62ae7e2cffad2ff0cc7d379dc3e283c9cb0dd790
Divyansh Gupta <divyanshgupta6170@gmail.com>
2025-11-11 19:45:42 +0530

    setup nextjs

---
+++ .vscode/x.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+addition=a+b
+multiplication=a*b
+division=a/b
+substraction=a-b
+print(addition)
+print(multiplication)
+print(division)
+print(substraction)

e38b889dc5d6fe6f4c4c119a2d5d67218c42b231
Divyansh Gupta <divyanshgupta6170@gmail.com>
2025-11-12 01:33:55 +0530

    reorganised the components

--- src/app/(home)/components/Banner.tsx
+++ src/app/(home)/components/Banner.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import BookImage from "../../public/book_image.png";
-import PaperBg from "../../public/paper-bg.jpg";
 
 const Banner = () => {
   return (
     <div className="mx-auto max-w-7xl px-5 py-10">
       <div className="relative">
         <Image
-          src={PaperBg}
+          src={"/paper-bg.jpg"}
           alt="billboard"
           className="h-72 w-full rounded-lg"
           height={0}
@@ -17,7 +15,7 @@ const Banner = () => {
         />
         <div className="absolute inset-0 h-full w-full rounded-lg bg-gray-950 opacity-30" />
         <Image
-          src={BookImage}
+          src={"/book_image.png"}
           alt="Billboard"
           className="absolute bottom-0 right-5"
           height={0}
